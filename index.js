725a0e2cccd940b55f1a9ce249d2f0d1b9358d34
deanveb <Annhi1975@gmail.com>
2025-07-13 08:55:40 +0700

    change to vercel

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-const express = require('express');
-const app = express();
-const port = 3000;
+// app.get('/weather', async (req, res) => {
+//   res.status(200).json({ message: `Hello!` });
+// })
 
-app.get('/weather', async (req, res) => {
-  res.status(200).json({ message: `Hello!` });
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+export default async function handler(req, res) {
+  return res.status(200).json({
+    message: "hello world"
+  })
+}
